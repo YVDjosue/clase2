0cac6833c4f5c3da78fc5f49e34f362486101a5e
YVDjosue <97265204+YVDjosue@users.noreply.github.com>
2024-09-18 00:02:28 +0000

    testing codespaces

--- josuepokemon.php
+++ josuepokemon.php
@@ -8,9 +8,6 @@ class JosuePokemon extends Pokemon implements iPokemon{
     }
 
    //Metodos de la interface iPokemon, USEN SU CREATIVIDAD
-   public function gritoBatalla():void{
-    echo "GAAAAAA<br>";
-   }
 
    public function getGolpeCritico():float{
     return $this->ataque * 2;
@@ -18,6 +15,18 @@ class JosuePokemon extends Pokemon implements iPokemon{
    
    public function aumentaDefensa(int $incremento):void{
     $this->defensa += $incremento;
+   }
+
+   public function reduceDefensa(int $decremento):void{
+    $this->defensa -= $decremento;
+   }
+
+   public function aumentaHp(int $incremento):void{
+    $this->hp += $incremento;
+   }
+
+   public function reduceHp(int $decremento):void{
+    $this->hp -= $decremento;
    }
 }
 
